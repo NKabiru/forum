44e7f68be3e01224b6d4c064e33666efeb666b81
NKabiru <njengakabiru@gmail.com>
2018-05-07 08:54:13 +0000

    Add Ep.3:A Thread Can Have Replies

---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReadThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_thread()
+    {
+        $this->get('/threads/'. $this->thread->id)
+            ->assertSee($this->thread->title);
+    }
+
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_replies_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)
+            ->create(['thread_id' => $this->thread->id]);
+
+        $this->get('/threads/'. $this->thread->id)
+            ->assertSee($reply->body);
+    }
+}
