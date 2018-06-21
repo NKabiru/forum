1e7732b3f60faee670b58e5f3512b978425943de
NKabiru <njengakabiru@gmail.com>
2018-06-21 12:15:23 +0000

    Add Ep.11: Users Can Filter Threads By Channel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,20 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li><a class="nav-link" href="/threads">All Threads</a></li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Channels
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @forelse(\App\Channel::all() as $channel)
+                                    <a class="dropdown-item" href="/threads/{{ $channel->slug }}">{{ $channel->name }}</a>
+                                @empty
+                                    <a class="dropdown-item text-muted" href="#">No channels</a>
+                                @endforelse
+                            </div>
+                        </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('threads', 'ThreadController@index');
 Route::get('threads/create', 'ThreadController@create');
+Route::get('threads/{channel}', 'ThreadController@index');
 Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::post('threads', 'ThreadController@store');
-
 //Route::resource('threads', 'ThreadController');
 
 Route::post('/threads/{channel}/{thread}/replies', 'ReplyController@store');
