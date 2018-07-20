2252e9efaf1c0c482794c692247e8cfdc5c000c4
NKabiru <njengakabiru@gmail.com>
2018-07-20 13:27:29 +0000

    Add Ep.33: Ajaxifying the Delete Button

--- resources/views/threads/replies.blade.php
+++ resources/views/threads/replies.blade.php
@@ -36,11 +36,7 @@
         @can('update', $reply)
             <div class="card-footer level">
                 <button class="btn btn-sm mr-1" @click="editing = true">Edit</button>
-                <form method="POST" action="/replies/{{ $reply->id }}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm">Delete</button>
-                </form>
+                <button class="btn btn-danger btn-sm" @click="destroy">Delete</button>
             </div>
         @endcan
     </div>
