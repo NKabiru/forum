fe256554c01cd51683a37a88214ff486ad6adff2
NKabiru <njengakabiru@gmail.com>
2018-08-14 13:16:43 +0000

    Add Ep.41: Thread Subscriptions: Part 2

---
+++ app/Http/Controllers/ThreadSubscriptionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Thread;
+
+class ThreadSubscriptionController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store($channelId, Thread $thread)
+    {
+        $thread->subscribe();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::get('threads/{channel}/{thread}', 'ThreadController@show');
 Route::delete('threads/{channel}/{thread}', 'ThreadController@destroy');
 Route::post('threads', 'ThreadController@store');
 
+Route::post('threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionController@store');
+
 Route::post('replies/{reply}/favorites', 'FavoriteController@store');
 Route::delete('replies/{reply}/favorites', 'FavoriteController@destroy');
 
