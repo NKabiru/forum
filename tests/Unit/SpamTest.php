3ecf1769bd1826c21ccb55cdded948c048a69c3f
NKabiru <njengakabiru@gmail.com>
2018-09-12 16:28:49 +0300

    Add Ep.50: Graduating Inspection Methods to Classes

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use App\Spam;
+use App\Inspections\Spam;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -12,10 +12,25 @@ class SpamTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function it_validates_spam()
+    public function it_checks_for_invalid_keywords()
     {
         $spam = new Spam();
 
         $this->assertFalse($spam->detect('Innocent text'));
+
+        $this->withoutExceptionHandling()->expectException(\Exception::class);
+
+        $spam->detect('yahoo customer support');
     }
+
+    /** @test */
+    public function it_checks_for_any_key_being_held_down()
+    {
+        $spam = new Spam();
+
+        $this->withoutExceptionHandling()->expectException(\Exception::class);
+
+        $spam->detect('Hello World aaaaaa');
+    }
+
 }
