c50ff8c5b488f3c4c16284fe1a85f97766a133f8
NKabiru <njengakabiru@gmail.com>
2018-11-07 05:48:00 +0300

    Prevent vue component from refreshing page when performing update

--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -19,7 +19,7 @@
         </div>
         <div class="card-body">
             <div v-if="editing">
-                <form @submit="update">
+                <form @submit.prevent="update">
                     <div class="form-group">
                         <textarea class="form-control" v-model="body" required></textarea>
                     </div>
