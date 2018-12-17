812e607bcff6f7d1e2ae249560263ad935b233fd
NKabiru <njengakabiru@gmail.com>
2018-12-17 23:43:06 +0300

    Add ep.93: A Thread Can Be Updated: Part 3

--- resources/views/threads/_topic.blade.php
+++ resources/views/threads/_topic.blade.php
@@ -3,14 +3,14 @@
     <div class="card-header">
         <div class="level">
             <h5 class="flex">
-                <input type="text" placeholder="{{ $thread->title }}" class="form-control">
+                <input type="text" class="form-control" v-model="form.title">
             </h5>
         </div>
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
@@ -18,8 +18,8 @@
 
         <div class="level">
             <button class="btn btn-sm mr-1" @click="editing = true" v-show="! editing">Edit</button>
-            <button class="btn btn-primary btn-sm mr-1" @click="">Update</button>
-            <button class="btn btn-sm" @click="editing = false">Cancel</button>
+            <button class="btn btn-primary btn-sm mr-1" @click="update">Update</button>
+            <button class="btn btn-sm" @click="resetForm">Cancel</button>
 
             @can('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-auto">
@@ -39,16 +39,14 @@
             <img src="{{ $thread->creator->avatar_path}}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-2">
             <h5 class="flex">
                 <a href="{{ route('profile',  $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                {{ $thread->title }}
+                <span v-text="title"></span>
             </h5>
         </div>
     </div>
 
-    <div class="card-body">
-        {{ $thread->body }}
-    </div>
+    <div class="card-body" v-text="body"></div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-sm" @click="editing = true">Edit</button>
     </div>
 </div>
